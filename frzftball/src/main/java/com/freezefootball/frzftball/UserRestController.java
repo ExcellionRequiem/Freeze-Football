fb568c0bd91e0afa30e6fe458add18a13b3fbe4c
ExcellionRequiem <130091012+ExcellionRequiem@users.noreply.github.com>
2024-12-17 17:56:37 +0100

    123

--- frzftball/src/main/java/com/freezefootball/frzftball/UserRestController.java
+++ frzftball/src/main/java/com/freezefootball/frzftball/UserRestController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpHeaders;
+//
 
 @Controller
 public class UserRestController {
